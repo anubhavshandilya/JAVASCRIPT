9a5363688a32d3c6d8cdd77497adb4153cbfdbe0
anubhavshandilya <aanubhavsinha7870@gmail.com>
2024-06-13 04:23:59 +0000

    dataTypes commit

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+console.log("Anubhav")
+
+
+let name = "Anubhav" //string data type
+let age = 19 //number data type
+let isLoggedIn = false //boolean data type
+
+//[PRIMITIVE DATA TYPES]
+// number => 2 to power 53
+//bigint {generally not used but in case there is used which exceeds the power 53; trading website, facebook,etc}
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => used for the case of uniqueness, mostly used in React for making various components, figma, etc.
+
+
+//object
+
+console.log(typeOf undefined); //its type is undefined
+console.log(typeOf null,); //it is defined as object
