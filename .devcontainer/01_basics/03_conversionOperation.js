c44179699d7850df483e9c523390d564a1c5e635
anubhavshandilya <aanubhavsinha7870@gmail.com>
2024-06-14 07:28:36 +0000

    Conversion of Data types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Anubhav"
+
+//console.log(typeOf score);
+//console.log(typeOf(score));
+
+let valueInNumber = Number(score)
+//console.log(typeOf valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan(Not a number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isBooleanLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Anubhav" => true
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+/*console.log(typeOf stringNumber)*/
