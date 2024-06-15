0ae258561ff931832426426e0ab01d669aab9f7c
anubhavshandilya <aanubhavsinha7870@gmail.com>
2024-06-15 08:05:54 +0000

    Operations of operators

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -23,5 +23,43 @@ console.log(isBooleanLoggedIn);
 let someNumber = 35
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-/*console.log(typeOf stringNumber)*/
+// console.log(stringNumber);
+/*console.log(typeOf stringNumber)
+
+/*---------Operations-------------*/
+let value = 30;
+let negValue = -value
+// console.log(negValue);]
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4); [power]
+// console.log(2/5);
+// console.log(2%7);
+
+//To add strings
+let str1 = "hello"
+let str2 = " anubhav"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//Postfix and prefix operators in js; reference- MDN docs
+// 1. If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// 2. If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
